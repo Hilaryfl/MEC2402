c10d22344beccc9b51bfc553e4a36316e44b63ba
Hilaryfl <143563514+Hilaryfl@users.noreply.github.com>
2025-05-01 16:41:55 +1000

    Update finalCode.cpp

--- finalCode.cpp
+++ finalCode.cpp
@@ -6,8 +6,8 @@
 #define M4_PWM 11
 #define M4_DIR 13
 
-// button setup
-const int buttonPin = 22;  // button input
+// Button setup
+const int buttonPin = 38;  // button input (now using internal pull-up)
 
 // Servo setup
 Servo bigServo;
@@ -15,35 +15,34 @@ Servo smallServo1;
 Servo smallServo2;
 
 int pos = 0;
-int duty = 0;    // variable to store duty cycle which controls the motor's effective voltage (range 0-255)
-
+int duty = 0;
 
 void setup() {
-  pinMode(buttonPin, INPUT);   // Read button state
-
+  pinMode(buttonPin, INPUT_PULLUP); // Enable internal pull-up resistor
   bigServo.attach(9);
   smallServo1.attach(10);
-  smallServo2.attach(11);
+  smallServo2.attach(6);
 
   pinMode(M2_PWM, OUTPUT);
   pinMode(M2_DIR, OUTPUT);
   pinMode(M4_PWM, OUTPUT);
   pinMode(M4_DIR, OUTPUT);
 
-  Serial.begin(9600);          // Start the serial monitor
+  Serial.begin(9600);
 }
 
 void loop() {
   int buttonState = digitalRead(buttonPin);
 
+  // Since pull-up is enabled, button is pressed when LOW
   if (buttonState == HIGH) {
     mainFunction();     
   } else {
-    Serial.println("im broken :(");
+    Serial.println("Waiting for button press...");
   }
 }
 
-void mainFunction() { // CHANGE TO FINAL CODE
+void mainFunction() {
   for (pos = 0; pos <= 90; pos++) {
     bigServo.write(pos);
     delay(15);
@@ -54,48 +53,48 @@ void mainFunction() { // CHANGE TO FINAL CODE
     smallServo2.write(pos);
     delay(15);
   }
-  
-  digitalWrite(M2_DIR, HIGH); 
-  digitalWrite(M4_DIR, HIGH); 
-  delay(10);
-
-
-  digitalWrite(M2_DIR, LOW); 
-  digitalWrite(M4_DIR, LOW); 
-  delay(10);
-
-  for (pos = 90; pos >= 0; pos--) {
-    smallServo1.write(pos);
-    smallServo2.write(pos);
-    delay(15);
-  }
-
-  for (pos = 90; pos >= 0; pos--) {
-    bigServo.write(pos);
-    delay(15);
-  }
-
-// ROTATES 
-  digitalWrite(M2_DIR, HIGH); 
-  digitalWrite(M4_DIR, LOW); 
-  delay(5);
 
   digitalWrite(M2_DIR, HIGH); 
   digitalWrite(M4_DIR, HIGH); 
-  delay(10);
+  analogWrite(M2_PWM, 255); 
+  analogWrite(M4_PWM, 255);
+  delay(4000);
 
-// stop to allow seesaw to tilt
-  analogWrite(M2_PWM, 0); // Stop motor M2
-  analogWrite(M4_PWM, 0); // Stop motor M4
+  analogWrite(M2_PWM, 0); 
+  analogWrite(M4_PWM, 0);
+  delay(4000);
 
-  digitalWrite(M2_DIR, HIGH); 
-  digitalWrite(M4_DIR, HIGH); 
-  delay(10);
+  for (pos = 90; pos >= 0; pos--) {
+    smallServo1.write(pos);
+    smallServo2.write(pos);
+    delay(15);
+  }
+
+  for (pos = 90; pos >= 0; pos--) {
+    bigServo.write(pos);
+    delay(15);
+  }
 
   digitalWrite(M2_DIR, HIGH); 
   digitalWrite(M4_DIR, LOW); 
-  delay(5);
-  
+  analogWrite(M2_PWM, 255); 
+  analogWrite(M4_PWM, 255);
+  delay(4000);
+
+  digitalWrite(M2_DIR, HIGH); 
+  digitalWrite(M4_DIR, HIGH); 
+  analogWrite(M2_PWM, 255); 
+  analogWrite(M4_PWM, 255);
+  delay(4000);
+
+  analogWrite(M2_PWM, 0); 
+  analogWrite(M4_PWM, 0); 
+  delay(4000);
+
+  digitalWrite(M2_DIR, HIGH); 
+  digitalWrite(M4_DIR, LOW); 
+  delay(4000);
+
   for (pos = 0; pos <= 90; pos++) {
     bigServo.write(pos);
     delay(15);
@@ -117,5 +116,4 @@ void mainFunction() { // CHANGE TO FINAL CODE
     bigServo.write(pos);
     delay(15);
   }
-
 }
